c3cdb538c0940377f89996bae76c4515b9914722
Vishal Pawar <openstack.vishal@gmail.com>
2021-10-26 02:13:16 +0530

    Added MS Test Project. Added Moq and Shouldly nuget references.

---
+++ VaccineTrackerDemoTests/ViewModelTests/MainViewModelFacts.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccineTrackerDemo.Common.DataProvider;
+using VaccineTrackerDemo.ViewModel;
+
+namespace VaccineTrackerDemoTests.ViewModelTests
+{
+    [TestClass]
+    public class MainViewModelFacts
+    {
+        private readonly Mock<IVaccineUserDataProvider> mockVaccineUserDataProvider;
+
+        public MainViewModelFacts()
+        {
+            this.mockVaccineUserDataProvider = new Mock<IVaccineUserDataProvider>();
+        }
+
+        [TestMethod]
+        public void Ctor_Gets_Created()
+        {
+            var vm = GetMainViewModel();
+            vm.ShouldNotBeNull();
+        }
+
+        private object GetMainViewModel()
+        {
+            return new MainViewModel(this.mockVaccineUserDataProvider.Object);
+        }
+    }
+}
